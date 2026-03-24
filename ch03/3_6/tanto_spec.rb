89f93e82e0d32e46ddd621e8bdc5594ffa5d0cc2
djkazunoko <djkazunoko@gmail.com>
2026-03-24 18:20:13 +0900

    3_6 Tanto#calculate_salaryのテスト名を英語に変更

--- ch03/3_6/tanto_spec.rb
+++ ch03/3_6/tanto_spec.rb
@@ -3,7 +3,7 @@ require_relative 'tanto'
 describe Tanto do
   let(:tanto) { Tanto.new}
 
-  example '担当の給料は基本給と一緒。基本給が100なら給料も100' do
+  it 'returns kyuryo as same as kihonkyu. if kihonkyu is 100, and then kyuryo equals 100' do
     expect(tanto.calculate_salary(100)).to eq 100
   end
 end
