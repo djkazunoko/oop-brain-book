bc8aafbe35cc08734d69eb7b4fa0299db74b9f29
djkazunoko <djkazunoko@gmail.com>
2026-03-25 20:31:08 +0900

    3_7 Torishimariyaku#standupを追加

--- ch03/3_7/torishimari_spec.rb
+++ ch03/3_7/torishimari_spec.rb
@@ -6,4 +6,8 @@ describe Torishimariyaku do
   example '取締役の給料は基本給の4倍+2なので、基本給が100なら給料は402' do
     expect(torishimariyaku.calculate_salary(100)).to eq 402
   end
+
+  example '取締役はふんぞり返って立ち上がる' do
+    expect(torishimariyaku.standup).to eq 'ふんぞり返って立ち上がりました。'
+  end
 end
--- ch03/3_7/torishimariyaku.rb
+++ ch03/3_7/torishimariyaku.rb
@@ -4,4 +4,8 @@ class Torishimariyaku < Shain
   def calculate_salary(kihonkyu)
     kihonkyu * 4 + 2
   end
+
+  def standup
+    'ふんぞり返って立ち上がりました。'
+  end
 end
